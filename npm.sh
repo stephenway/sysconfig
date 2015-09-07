4b961d75115d468c8509a1ed86b22ddc3068c3bc
Stephen Way <way.stephen@gmail.com>
2015-09-07 11:43:42 -0700

    Modified npm.sh with sudo for specific packages

--- npm.sh
+++ npm.sh
@@ -5,20 +5,20 @@ sudo -v
 
 sudo gem install scss_lint
 npm install -g babel
-npm install -g bower
+sudo npm install -g bower
 npm install -g browserify
 npm install -g browser-sync
 npm install -g casperjs
 npm install -g component
-npm install -g eslint
+sudo npm install -g eslint
 npm install -g ember-cli
 npm install -g gulp
-npm install -g harp
+sudo npm install -g harp
 npm install -g phantomcss
 npm install -g phantomjs
 npm install -g psi
 npm install -g slimerjs
 npm install -g vtop
-npm install -g webpack
+sudo npm install -g webpack
 npm install -g webpack-dev-server
 npm install -g yo
