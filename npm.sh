ff37b6e67291f5abcb95ec8cd4ad7d98e16a7cad
Stephen Way <way.stephen@gmail.com>
2015-09-07 12:56:29 -0700

    Added imageoptim-cli and shorthand install subcommand

--- npm.sh
+++ npm.sh
@@ -3,22 +3,23 @@
 # Ask for the administrator password upfront.
 sudo -v
 
-sudo gem install scss_lint
-sudo npm install -g babel
-sudo npm install -g bower
-sudo npm install -g browserify
-sudo npm install -g browser-sync
-sudo npm install -g casperjs
-sudo npm install -g component
-sudo npm install -g eslint
-sudo npm install -g ember-cli
-sudo npm install -g gulp
-sudo npm install -g harp
-sudo npm install -g phantomcss
-sudo npm install -g phantomjs
-sudo npm install -g psi
-sudo npm install -g slimerjs
-sudo npm install -g vtop
-sudo npm install -g webpack
-sudo npm install -g webpack-dev-server
-sudo npm install -g yo
+sudo gem i scss_lint
+sudo npm i -g babel
+sudo npm i -g bower
+sudo npm i -g browserify
+sudo npm i -g browser-sync
+sudo npm i -g casperjs
+sudo npm i -g component
+sudo npm i -g eslint
+sudo npm i -g ember-cli
+sudo npm i -g gulp
+sudo npm i -g harp
+sudo npm i -g imageoptim-cli
+sudo npm i -g phantomcss
+sudo npm i -g phantomjs
+sudo npm i -g psi
+sudo npm i -g slimerjs
+sudo npm i -g vtop
+sudo npm i -g webpack
+sudo npm i -g webpack-dev-server
+sudo npm i -g yo
