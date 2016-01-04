2d8f7a3f38c8be8950ee24d11561285e6b09bd51
Stephen Way <way.stephen@gmail.com>
2016-01-04 09:57:52 -0800

    Added new npm packages

--- npm.sh
+++ npm.sh
@@ -4,23 +4,30 @@
 sudo -v
 
 sudo npm i -g a11y
+sudo npm i -g autoprefixer
 sudo npm i -g babel
 sudo npm i -g bower
 sudo npm i -g browserify
 sudo npm i -g browser-sync
 sudo npm i -g casperjs
+sudo npm i -g cli-github
 sudo npm i -g component
 sudo npm i -g eslint
-sudo npm i -g ember-cli
+sudo npm i -g ghcal
 sudo npm i -g gulp
 sudo npm i -g harp
 sudo npm i -g imageoptim-cli
+sudo npm i -g jsonlint
+sudo npm i -g pakmanager
 sudo npm i -g perfbudget
 sudo npm i -g phantomcss
 sudo npm i -g phantomjs
+sudo npm i -g postcss-cli
 sudo npm i -g psi
 sudo npm i -g slimerjs
-sudo npm i -g updtrs
+sudo npm i -g speed-test
+sudo npm i -g svgo
+sudo npm i -g updtr
 sudo npm i -g vtop
 sudo npm i -g webpack
 sudo npm i -g webpack-dev-server
