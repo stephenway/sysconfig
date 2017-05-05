c0228f6554968bbb5bc901e046569f5bbe018e1f
Stephen Way <way.stephen@gmail.com>
2017-05-05 14:59:56 -0700

    Updated npm dependencies

--- npm.sh
+++ npm.sh
@@ -2,30 +2,31 @@
 
 set -e
 
-yarn global add a11y
-yarn global add autoprefixer
-yarn global add babel
-yarn global add babel-eslint
-yarn global add bower
-yarn global add browserify
-yarn global add browser-sync
-yarn global add cli-github
-yarn global add diff-so-fancy
-yarn global add eslint
-yarn global add eslint-plugin-react
-yarn global add ghcal
-yarn global add gulp
-yarn global add harp
-yarn global add imageoptim-cli
-yarn global add jsonlint
-yarn global add nativefier
-yarn global add pakmanager
-yarn global add perfbudget
-yarn global add postcss-cli
-yarn global add psi
-yarn global add slimerjs
-yarn global add speed-test
-yarn global add svgo
-yarn global add updtr
-yarn global add vtop
-yarn global add web-ext
+sudo yarn global add a11y
+sudo yarn global add autoprefixer
+sudo yarn global add babel
+sudo yarn global add babel-eslint
+sudo yarn global add bower
+sudo yarn global add browserify
+sudo yarn global add browser-sync
+sudo yarn global add cli-github
+sudo yarn global add david
+sudo yarn global add diff-so-fancy
+sudo yarn global add eslint
+sudo yarn global add eslint-plugin-react
+sudo yarn global add ghcal
+sudo yarn global add gulp
+sudo yarn global add harp
+sudo yarn global add imageoptim-cli
+sudo yarn global add jsonlint
+sudo yarn global add nativefier
+sudo yarn global add pakmanager
+sudo yarn global add perfbudget
+sudo yarn global add postcss-cli
+sudo yarn global add psi
+sudo yarn global add slimerjs
+sudo yarn global add speed-test
+sudo yarn global add svgo
+sudo yarn global add updtr
+sudo yarn global add vtop
+sudo yarn global add web-ext
